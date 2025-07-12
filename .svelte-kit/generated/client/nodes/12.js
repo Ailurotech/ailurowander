5ac1b345247ed0370dcafd56b07cadc5b65ffbf6
Max Zhuge <zhugeruntao@outlook.com>
2025-07-13 03:21:56 +0930

    remove svelt kit

--- .svelte-kit/generated/client/nodes/12.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/tours/[name]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/tours/[name]/+page.svelte";
--- .svelte-kit/generated/client/nodes/13.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/tours/by-id/[id]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/tours/by-id/[id]/+page.svelte";
